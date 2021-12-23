ce8379e818e5081cf74f13c4b083825bff2fb223
Unfreeman <33789797+Unfreeman@users.noreply.github.com>
2021-12-23 20:35:56 +0800

    Update and rename ball_calc_release_ver0.py to ball_calc_release.py
    Bug fix and add feature

--- ball_calc_release.py
+++ ball_calc_release.py
@@ -11,10 +11,12 @@ end_alt = 0
 # meter
 speed = 70
 # m/s
-temperature = 15
+temperature = 0
 # celsius
 pressure = 1013.25
 # hPa
+angle = 0
+# degree
 dragco = 0.5
 g = 9.8
 
@@ -22,8 +24,8 @@ airde = pressure * 100 / (287.05 * (temperature + 273.15))
 sect = diameter / 2 / 1000 * diameter / 2 / 1000 * np.pi
 calc_leap = calc_leap / 1000
 weight = weight / 1000
-x_speed = speed
-y_speed = 0
+x_speed = np.cos(angle*np.pi/180)*speed
+y_speed = np.sin(angle*np.pi/180)*speed
 track = np.array([[0], [start_alt], [x_speed], [y_speed]])
 
 
@@ -43,12 +45,19 @@ calc_count = 0
 while track[1, calc_count] > end_alt:
     drag_speed = np.sqrt(np.square(track[2, calc_count]) + np.square(track[3, calc_count]))
     drag_force = drag(drag_speed)
-    x_acc = np.cos(drag_force * np.pi / 180) * drag_force / weight
-    y_acc = np.sin(drag_force * np.pi / 180) * drag_force / weight + g
-    x_calc_leap = move(track[2, calc_count], -x_acc)
+    angle_calc = np.arctan(track[3,calc_count]/track[2,calc_count])
+    x_acc = np.cos(angle_calc) * drag_force / weight
+    y_acc = np.sin(angle_calc) * drag_force / weight
+    if track[2, calc_count] >= 0:
+        x_calc_leap = move(track[2, calc_count], -x_acc)
+    else:
+        x_calc_leap = move(track[2, calc_count], x_acc)
     x_move_leap = track[0, calc_count] + x_calc_leap[0]
     x_speed_leap = x_calc_leap[1]
-    y_calc_leap = move(track[3, calc_count], -y_acc)
+    if track[3, calc_count] >= 0:
+        y_calc_leap = move(track[3, calc_count], -y_acc - g)
+    else:
+        y_calc_leap = move(track[3, calc_count], y_acc - g)
     y_move_leap = track[1, calc_count] + y_calc_leap[0]
     y_speed_leap = y_calc_leap[1]
     track = np.append(track, [[x_move_leap], [y_move_leap], [x_speed_leap], [y_speed_leap]], axis=1)
